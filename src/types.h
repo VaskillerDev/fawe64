4a53adbb5c72820b28829614745f9c85267d1ab6
VaskillerDev <Vaskoooo9241@gmail.com>
2022-01-18 23:27:56 +0300

    minor

--- src/types.h
+++ src/types.h
@@ -1,6 +1,10 @@
-#include"wasm4.h"
-
+#pragma once
+#include "wasm4.h"
 typedef int32_t int_32;
 typedef uint32_t uint_32;
 typedef uint16_t uint_16;
 typedef uint8_t uint_8;
+
+typedef struct MenuState MenuState;
+typedef struct GameState GameState;
+typedef struct EventEmitter EventEmitter;
