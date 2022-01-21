2856ab22a404d831c4aeb6eda0463d462c2b4844
VaskillerDev <Vaskoooo9241@gmail.com>
2022-01-21 14:15:22 +0300

    +tiles

--- src/image_pool.c
+++ src/image_pool.c
@@ -2,8 +2,8 @@
 
 void imagePool_init(ImagePool *pool)
 {
-    pool->imageCount = 4;
-    pool->data = malloc(sizeof(uint8_t) * (64 * 3 + 2560));
+    pool->imageCount = 5;
+    pool->data = malloc(sizeof(uint8_t) * (32 * 3 + 64 * 8 + 2560));
     pool->images = (Image **)malloc(sizeof(Image) * pool->imageCount);
     pool->offset = 0;
 
@@ -11,6 +11,15 @@ void imagePool_init(ImagePool *pool)
     *(pool->images + 1) = imagePool_addImage(pool,(void*) piligrim_idle_b_img_0, 64, 16, 16, menuImgColors, BLIT_2BPP);
     *(pool->images + 2) = imagePool_addImage(pool, (void*)piligrim_idle_b_img_1, 64, 16, 16, menuImgColors, BLIT_2BPP);
     *(pool->images + 3) = imagePool_addImage(pool, (void*)piligrim_idle_b_img_2, 64, 16, 16, menuImgColors, BLIT_2BPP);
+
+    *(pool->images + 4) = imagePool_addImage (pool, (void*)tile0, 64, 16, 16, menuImgColors, BLIT_2BPP);
+    *(pool->images + 5) = imagePool_addImage (pool, (void*)tile1, 64, 16, 16, menuImgColors, BLIT_2BPP);
+    *(pool->images + 6) = imagePool_addImage (pool, (void*)tile2, 32, 16, 16, menuImgColors, BLIT_1BPP);
+    *(pool->images + 7) = imagePool_addImage (pool, (void*)tile3, 32, 16, 16, menuImgColors, BLIT_1BPP);
+    *(pool->images + 8) = imagePool_addImage (pool, (void*)tile4, 64, 16, 16, menuImgColors, BLIT_2BPP);
+    *(pool->images + 9) = imagePool_addImage (pool, (void*)tile5, 32, 16, 16, menuImgColors, BLIT_1BPP);
+    *(pool->images + 10) = imagePool_addImage (pool, (void*)tile6, 64, 16, 16, menuImgColors, BLIT_2BPP);
+    *(pool->images + 11) = imagePool_addImage (pool, (void*)tile7, 64, 16, 16, menuImgColors, BLIT_2BPP);
 }
 
 Image *imagePool_addImage(ImagePool *pool, void *dataPtr, uint_32 size, uint_32 w, uint_32 h, uint_16 colors[4], uint_32 flags)
--- src/image_pool.h
+++ src/image_pool.h
@@ -9,7 +9,25 @@ struct ImagePool
     uint_32 offset;
 };
 
-typedef struct ImagePool ImagePool;
+/**
+ * Индексы изображений в пуле изображений
+ */
+typedef enum ImagePoolIndex {
+    PoolIdx_MenuImage,
+
+    PoolIdx_PiligrimIdleBottom0,
+    PoolIdx_PiligrimIdleBottom1,
+    PoolIdx_PiligrimIdleBottom2,
+
+    PoolIdx_Tile0,
+    PoolIdx_Tile1,
+    PoolIdx_Tile2,
+    PoolIdx_Tile3,
+    PoolIdx_Tile4,
+    PoolIdx_Tile5,
+    PoolIdx_Tile6,
+    PoolIdx_Tile7
+} ImagePoolIndex;
 
 void imagePool_init(ImagePool* pool);
 Image* imagePool_addImage(ImagePool* pool, void* dataPtr, uint_32 size, uint_32 w, uint_32 h, uint_16 colors[4], uint_32 flags);
