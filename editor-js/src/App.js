5f697de3937c069dbb6b625d46981cdf707c5ade
VaskillerDev <Vaskoooo9241@gmail.com>
2022-06-24 03:26:14 +0300

    update

--- editor-js/src/App.js
+++ editor-js/src/App.js
@@ -4,7 +4,9 @@ import GridViewGl from "./gl/GridViewGl";
 function App() {
   return (
     <div className="App">
+        <header className={"App-header"}>
         <GridViewGl/>
+        </header>
     </div>
   );
 }
--- editor-js/src/gl/GridViewGl.js
+++ editor-js/src/gl/GridViewGl.js
@@ -4,6 +4,7 @@ import {Viewport} from "pixi-viewport";
 
 const MAP_VIEW_SIZE = 2048
 const ZOOM_FACTOR = 2;
+const BLOCK_SIZE = 16;
 
 export default class GridViewGl extends Component {
     
@@ -19,62 +20,52 @@ export default class GridViewGl extends Component {
     
     componentDidMount() {
         console.log("mounted");
-        this.app = new PIXI.Application();
-        /*const gridViewGlElement = document.getElementById("gridViewGl");
-        const ctx = gridViewGlElement.getContext('2d');
-        ctx.imageSmoothingEnabled = false;
-        
-        const img = new Image();
-        img.onload = function () {
-            ctx.drawImage(img, 0, 0, 16, 16, 0 ,0 , 16, 16);
-            
-        }
-        img.src = 'tiles.png'
-
-        ctx.scale(ZOOM_FACTOR,ZOOM_FACTOR);
-        
-        gridViewGlElement.addEventListener("mousemove", function (e) {
-            ctx.fillStyle = "#131618";
-            ctx.fillRect(0, 0, MAP_VIEW_SIZE, MAP_VIEW_SIZE);
-            
-            const mousePos = this.mousePos(gridViewGlElement, e);
-            console.log(mousePos.x + ',' + mousePos.y);
-            ctx.strokeStyle = "#ffffff";
-
-            mousePos.x /= ZOOM_FACTOR;
-            mousePos.y /= ZOOM_FACTOR;
-
-            let gridX = Math.ceil(mousePos.x / (16 * ZOOM_FACTOR));
-            let gridY = Math.ceil(mousePos.y / (16 * ZOOM_FACTOR));
-
-            gridX =  gridX > 0  ? gridX - 1 : gridX;
-            gridY = gridY > 0  ? gridY - 1 : gridY;
-            
-            console.log(gridX, gridY);
-            
-            ctx.strokeRect( gridX * 16, gridY * 16, 16, 16);
-
-            ctx.drawImage(img, 0, 0, 16, 16, 0 ,0 , 16, 16);
-        }.bind(this), false);
-
-        ctx.fillStyle = "#131618";
-        this.setState({
-            gridViewGlElement
-        })*/
-
+        this.app = new PIXI.Application({
+            width: MAP_VIEW_SIZE,
+            height: MAP_VIEW_SIZE,
+            view: document.getElementById("gridCanvas"),
+            backgroundColor:  0x2A2A2C,
+            clearBeforeRender: true
+        });
         
         document.getElementById("gridViewGl").appendChild(this.app.view)
 
         const viewport = new Viewport({
-            screenWidth: 3000,
-            screenHeight: 720,
+            screenWidth: MAP_VIEW_SIZE,
+            screenHeight: MAP_VIEW_SIZE,
             worldWidth: MAP_VIEW_SIZE,
             worldHeight: MAP_VIEW_SIZE,
-
+            
             interaction: this.app.renderer.plugins.interaction
         })
+        
+        const mouseCursorBlock = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
+        mouseCursorBlock.tint = 0xff0000
+        mouseCursorBlock.width = mouseCursorBlock.height = 16
+        mouseCursorBlock.position.set(0, 0)
 
+        const borderLine = new PIXI.Graphics();
+        const gridLine = new PIXI.Graphics();
+        viewport.addChild(borderLine);
+        viewport.addChild(gridLine);
+        
+        viewport.addListener("mousemove", (e)=> {
+            let mousePos = viewport.toWorld(e.data.global.x, e.data.global.y)
+            
+            if (Number.isNaN(mousePos.x)) return
+            
+            let gridX = Math.ceil(mousePos.x / 16);
+            let gridY = Math.ceil(mousePos.y / 16);
+
+            gridX =  gridX > 0  ? gridX - 1 : gridX;
+            gridY = gridY > 0  ? gridY - 1 : gridY
+
+            mouseCursorBlock.position.set(16 * gridX, 16 * gridY)
+        })
+        
+        
         this.app.stage.addChild(viewport)
+        
 
         viewport
             .drag()
@@ -82,10 +73,66 @@ export default class GridViewGl extends Component {
             .wheel()
             .decelerate()
 
+        viewport.fit()
+
         const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
         sprite.tint = 0xff0000
-        sprite.width = sprite.height = 100
-        sprite.position.set(100, 100)
+        sprite.width = sprite.height = 16
+        sprite.position.set(0, 0)
+
+
+        this.drawBorder(borderLine, viewport)
+        this.drawGrid(gridLine, viewport)
+    }
+    
+    drawBorder(line, viewport) {
+        line.clear();
+        this.drawLine(line, viewport,0,0, 0, MAP_VIEW_SIZE);
+        this.drawLine(line, viewport,0,0, MAP_VIEW_SIZE, 0);
+        this.drawLine(line, viewport,0,MAP_VIEW_SIZE, MAP_VIEW_SIZE, MAP_VIEW_SIZE);
+        this.drawLine(line, viewport, MAP_VIEW_SIZE,0, MAP_VIEW_SIZE, MAP_VIEW_SIZE);
+    }
+    
+    drawGrid(line, viewport) {
+        //line.clear();
+        const chunkSize = 8 * BLOCK_SIZE;
+
+        const viewportPointRB = viewport.toWorld(viewport.right, viewport.bottom)
+        
+        console.log(viewport.right)
+        
+        for (let x = 0; x < MAP_VIEW_SIZE; x+=BLOCK_SIZE) {
+            
+            //if (x >= viewport.right || x >= viewport.bottom ) continue;
+            
+            if (x % chunkSize === 0) {
+                this.drawLine(line, viewport, x, 0, x, MAP_VIEW_SIZE);
+                this.drawLine(line, viewport, 0, x, MAP_VIEW_SIZE, x);
+            } else {
+                this.drawGridLine(line, viewport, x, 0, x, MAP_VIEW_SIZE);
+                this.drawGridLine(line, viewport, 0, x, MAP_VIEW_SIZE, x);
+            }
+        }
+
+        
+    }
+    
+    drawLine(line, viewport,x0,y0,x1,y1) {
+        const widthLine = 1;
+        const color = 0xffffff
+        
+        line.lineStyle(widthLine, color, 1, 0.5,true)
+            .moveTo(x0, y0)
+            .lineTo(x1, y1);
+    }
+
+    drawGridLine(line, viewport,x0,y0,x1,y1) {
+        const widthLine = 1;
+        const color = 0xffffff
+        
+        line.lineStyle(widthLine, color, 0.2, 0.5,true)
+            .moveTo(x0, y0)
+            .lineTo(x1, y1);
         
     }
 
@@ -101,7 +148,9 @@ export default class GridViewGl extends Component {
         console.log("render")
         return (
             <Fragment>
-                <div id={"gridViewGl"} style={{width: 3000}}>
+                <div id={"gridViewGl"}  >
+                    <canvas id={"gridCanvas"}> 
+                    </canvas>
                 </div>
             </Fragment>
         );
