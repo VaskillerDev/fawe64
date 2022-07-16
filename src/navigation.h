0d373c377540d2e7710f8298142137dc05091b4f
Dr.Marx <AlexsSyr@mail.ru>
2022-07-16 23:30:57 +0300

    Исправлен баг "летающий меч"
    Исправлен баг "летающий меч"

--- src/navigation.h
+++ src/navigation.h
@@ -5,31 +5,10 @@ typedef Vec2 NavPoint;
 struct NavRoot
 {
     NavPoint navPointArray[16];
-    uint_32 navPointArraySize = 0;
-    uint_32 currentPointIndex = 0;
+    uint_32 navPointArraySize;
+    uint_32 currentPointIndex;
 };
 
-NavPoint *Navigation_GetCurrentNavPoint(NavRoot *const navRoot)
-{
-    return &navRoot->navPointArray[navRoot->currentPointIndex];
-}
-
-NavPoint *Navigation_NextNavPoint(NavRoot *const navRoot)
-{
-    navRoot->currentPointIndex = navRoot->currentPointIndex % navRoot->navPointArraySize;
-    return &navRoot->navPointArray[navRoot->currentPointIndex];
-}
-
-void Navigation_Move(Enemy *target, NavRoot *root, float moveSpeed)
-{
-    NavPoint *navPoint = Navigation_GetCurrentNavPoint(root);
-    Vec2 moveDir = vec2_div(navPoint, target->sprite->position);
-
-    if (vec2_getLength(moveDir) <= 2)
-    {
-        navPoint = Navigation_NextNavPoint(root);
-        moveDir = vec2_div(navPoint, target->sprite->position);
-    }
-
-    target->sprite->position = vec2_add(target->sprite->position, vec2_normalize(moveDir) * moveSpeed);
-}
+NavPoint *Navigation_GetCurrentNavPoint(struct NavRoot *const navRoot);
+NavPoint *Navigation_NextNavPoint(struct NavRoot *const navRoot);
+void Navigation_Move(struct Enemy *target, struct NavRoot *root, float moveSpeed);
